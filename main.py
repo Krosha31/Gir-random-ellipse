148a621ab46f2db7498f30cd8f8081c45c5d5922
Евгений <malkov.zhenja.ru@mail.ru>
2019-11-14 22:47:21 +0300

    a

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randrange
 import sys
-from PyQt5 import uic
+from ui_file import UI1.py
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
@@ -9,7 +9,6 @@ from PyQt5.QtGui import QPainter, QColor
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
         self.button.clicked.connect(self.zachem)
         self.krug = False
 
@@ -23,7 +22,7 @@ class Example(QMainWindow):
             qp.begin(self)
             rad = randrange(1, 301)
             x, y = randrange(1, 401), randrange(1, 401)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randrange(255), randrange(255), randrange(255)))
             qp.drawEllipse(x, y, rad, rad)
             qp.end()
             self.krug = False
